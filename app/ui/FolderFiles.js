a153f956ac0c13493f49b98847903cc28b2382f9
Muhammad Akash <grixxnixx@gmail.com>
2024-05-20 07:57:16 +0600

    Update #d635

--- app/ui/FolderFiles.js
+++ app/ui/FolderFiles.js
@@ -5,7 +5,7 @@ const FolderFiles = () => {
     <div>
       <h1 className="heading-h1 mt-12">NCM Moscow</h1>
 
-      <div className="folders grid grid-cols-4 gap-4 flex-wrap mt-12">
+      <div className="folders grid grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-4 flex-wrap mt-12">
         <button className="folder flex flex-col items-center flex-wrap">
           <PiFoldersThin size="100" color="#27272a" />
           <span className="text-sm font-medium">Educational Content</span>
