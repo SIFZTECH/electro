ab82de393c3380a87a901233f9c7daa8523ab66e
Muhammad Akash <grixxnixx@gmail.com>
2024-06-24 10:04:41 +0600

    Update product spec

--- app/dashboard/product-specifications/[slug]/ProductTop.js
+++ app/dashboard/product-specifications/[slug]/ProductTop.js
@@ -98,7 +98,7 @@ const ProductTop = ({ product }) => {
               {sizeValues.map((size, i) => (
                 <ToggleGroupItem
                   key={i + 1}
-                  className="px-3 cursor-pointer border border-color-primary py-1 hover:bg-transparent hover:text-color-primary data-[state='on']:bg-color-primary font-serif text-base"
+                  className="px-3 cursor-pointer border border-color-primary py-1 hover:bg-transparent hover:text-color-primary data-[state=on]:bg-color-primary font-serif text-base"
                   value={size}
                 >
                   {size}
