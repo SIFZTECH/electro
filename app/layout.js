255e43bd849a16978a46dd4cb15f7ba953f6ad15
Muhammad Akash <grixxnixx@gmail.com>
2024-06-29 15:49:52 +0600

    Update toast

--- app/layout.js
+++ app/layout.js
@@ -14,19 +14,27 @@ export default function RootLayout({ children }) {
       <body className="font-sans lg:flex overflow-x-hidden">
         <ReactQueryProvider>{children}</ReactQueryProvider>
         <Toaster
-          position="top-right"
+          position="bottom-right"
           toastOptions={{
             success: {
               style: { backgroundColor: "#22c55e", color: "#fff" },
+              // Make success toast closable
+              icon: true,
+              closeable: true,
+              duration: 2000,
             },
-            error: { style: { backgroundColor: "#ef4444", color: "#fff" } },
-            icon: false,
+            error: {
+              style: { backgroundColor: "#ef4444", color: "#fff" },
+              // Make error toast closable
+              icon: true,
+              closeable: true,
+              duration: 2000,
+            },
             style: {
               textAlign: "center",
               fontSize: "16px",
               borderRadius: "5px",
             },
-            duration: 3000,
           }}
         />
       </body>
