17fdb2e989b218919bc8b75d26f510b317869cd6
Muhammad Akash <grixxnixx@gmail.com>
2024-06-29 18:34:32 +0600

    Update #b89

--- app/_root_ui/FilterByProduct.js
+++ app/_root_ui/FilterByProduct.js
@@ -16,11 +16,7 @@ const FilterByProduct = () => {
     <div className="border border-gray-0 ">
       <div className="border-b flex justify-between gap-2 py-6 px-3 font-serif">
         <h2 className="font-semibold">Filters</h2>
-        <Link
-          href="/dashboard/product-specifications"
-          className="text-yellow-400"
-          onClick={resetFilters}
-        >
+        <Link href="/" className="text-yellow-400" onClick={resetFilters}>
           Clear All
         </Link>
       </div>
