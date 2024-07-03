068e512b8e8946d63fcf5bedf6ab751bef89ac06
Muhammad Akash <grixxnixx@gmail.com>
2024-07-03 07:41:35 +0600

    Update dashboard

--- app/dashboard/warranties/WarrantyStats.js
+++ app/dashboard/warranties/WarrantyStats.js
@@ -4,22 +4,21 @@ import Stat from "./WarrantyStat";
 const Stats = () => {
   const { data, isLoading, isError, error } = useWarrantyStats();
 
-  console.log(data);
   return (
     <>
       {!isLoading && !isError && !error && data && (
         <div className="flex flex-col gap-10 lg:grid lg:grid-cols-3 lg:gap-10">
           <Stat
             title="Month to Warranty Registration"
-            value={data.data.last_month_count}
+            value={data.data.last_month_count || 0}
           />
           <Stat
             title="Last 30 days Warranty Registration"
-            value={data.data.last_30_days_count}
+            value={data.data.last_30_days_count || 0}
           />
           <Stat
             title="Month to Warranty Registration"
-            value={data.data.last_12_months_count[0].count}
+            value={data?.data.last_12_months_count[0]?.count || 0}
           />
         </div>
       )}
