f2c6519fe52e9ead7923fd944916b36e61a9481a
Muhammad Akash <grixxnixx@gmail.com>
2024-07-04 07:33:37 +0600

    Update resend otp

--- app/dashboard/settings/SettingsForm.js
+++ app/dashboard/settings/SettingsForm.js
@@ -34,11 +34,7 @@ const SettingsForm = () => {
       phone: user.phone || "",
       company_name: user.company_name || "",
       weburl: user.weburl || "",
-      abn: user.abn || "",
-      purchase_date: user.purchase_date
-        ? new Date(user.purchase_date).toISOString().split("T")[0]
-        : "",
-      invoice_number: user.invoice_number || "",
+
       description: user.description || "",
       street_address: user.street_address || "",
       description: user.description || "",
@@ -46,22 +42,7 @@ const SettingsForm = () => {
       postal_code: user.postal_code || "",
       state: user.state || "",
       logo: user.logo || "",
-      stockfeedurl: user.stockfeedurl || "",
-      map_url: user.map_url || "",
-      weeks: user.weeks || [
-        {
-          day: "",
-          opening_hours: "0:00",
-          closing_hours: "0:00",
-          is_holiday: 0,
-        },
-      ],
     },
-  });
-
-  const { fields, append, remove } = useFieldArray({
-    control,
-    name: "weeks",
   });
 
   async function onSubmit({
@@ -71,7 +52,7 @@ const SettingsForm = () => {
     phone,
     company_name,
     weburl,
-    abn,
+
     purchase_date,
     invoice_number,
     description,
@@ -80,10 +61,6 @@ const SettingsForm = () => {
     postal_code,
     state,
     logo,
-    stockfeedurl,
-
-    map_url,
-    weeks,
   }) {
     const coords = getCoordinatesFromUrl(map_url);
 
@@ -247,47 +224,7 @@ const SettingsForm = () => {
               />
             </div>
           </div>
-          <div className="">
-            <label className="block text-sm font-semibold font-serif leading-6 text-gray-900">
-              ABN
-            </label>
-            <select
-              name="abn"
-              className="block w-full rounded-md border bg-gray-100 border-gray-300 py-1.5 px-3 text-gray-900 shadow-sm px-3placeholder:text-gray-400 sm:text-sm sm:leading-6"
-            >
-              <option value="">--Please choose an option--</option>
-              <option value="abn">ABN</option>
-              <option value="abn">TV5</option>
-            </select>
-          </div>
-          <div className="">
-            <label className="block text-sm font-semibold font-serif leading-6 text-gray-900 after:content-['*'] after:ml-0.5 after:text-red-600">
-              Purchase Date
-            </label>
-            <input
-              {...register("purchase_date", {
-                required: "Please pick your purchase date",
-              })}
-              type="date"
-              className="block w-full rounded-md border bg-gray-100 border-gray-300 py-1.5 px-3 text-gray-900 shadow-sm px-3placeholder:text-gray-400 sm:text-sm sm:leading-6"
-            />
-            {errors?.purchase_date && (
-              <span className="text-red-500 text-sm">
-                {errors.purchase_date.message}
-              </span>
-            )}
-          </div>
-          <div className="">
-            <label className="block text-sm font-semibold font-serif leading-6 text-gray-900">
-              Invoice Number
-            </label>
-            <div className="mt-1">
-              <input
-                {...register("invoice_number")}
-                className="block w-full rounded-md border bg-gray-100 border-gray-300 py-1.5 px-3 text-gray-900 shadow-sm px-3placeholder:text-gray-400 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
+
           <div className="">
             <label className="block text-sm font-semibold font-serif leading-6 text-gray-900">
               Description
@@ -377,168 +314,10 @@ const SettingsForm = () => {
               )}
             </div>
           </div>
-          <div className="">
-            <label className="block text-sm font-semibold font-serif leading-6 text-gray-900 after:content-['*'] after:ml-0.5 after:text-red-600">
-              Your Google Map Address url
-            </label>
-            <div className="mt-1">
-              <input
-                {...register("map_url", {
-                  required: "This is required field",
-                })}
-                type="url"
-                placeholder="Paste Google Maps URL here"
-                className="block w-full rounded-md border bg-gray-100 border-gray-300 py-1.5 px-3 text-gray-900 shadow-sm px-3placeholder:text-gray-400 sm:text-sm sm:leading-6"
-              />
-              {errors?.map_url && (
-                <span className="text-red-500 text-sm">
-                  {errors.map_url.message}
-                </span>
-              )}
-            </div>
-          </div>
 
-          <div className="col-span-2">
-            {fields.map((item, index) => (
-              <div
-                key={item.id}
-                className="flex flex-col md:flex-row gap-3 md:gap-8 md:items-center w-full "
-              >
-                <div className="flex flex-col md:flex-row gap-3 md:gap-8 md:items-center w-full">
-                  <div className="flex-1">
-                    <label className="block text-sm font-semibold font-serif leading-6 text-gray-900 mb-1 after:content-['*'] after:ml-0.5 after:text-red-600">
-                      Days
-                    </label>
-                    <Controller
-                      name={`weeks[${index}].day`}
-                      control={control}
-                      rules={{ required: "Day field is required" }}
-                      defaultValue={item.day}
-                      render={({ field }) => (
-                        <select
-                          className="block w-full rounded-md border bg-gray-100 border-gray-300 py-1.5 px-3 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6 disabled:cursor-not-allowed "
-                          {...field}
-                        >
-                          <option value="">Select Days</option>
-                          <option value="sun">Sun</option>
-                          <option value="mon">Mon</option>
-                          <option value="tue">Tue</option>
-                          <option value="wed">Wed</option>
-                          <option value="thu">Thu</option>
-                          <option value="fri">Fri</option>
-                          <option value="sat">Sat</option>
-                        </select>
-                      )}
-                    />
-                    {errors?.day?.[index]?.day && (
-                      <span className="text-red-500 text-sm self-center">
-                        {errors.day[index].day.message}
-                      </span>
-                    )}
-                  </div>
-                </div>
-                <div className="flex flex-col md:flex-row gap-3 md:gap-8 md:items-center w-full">
-                  <div className="flex-1">
-                    <label className="block text-sm font-semibold font-serif leading-6 text-gray-900 mb-1 after:content-['*'] after:ml-0.5 after:text-red-600">
-                      Is Holiday
-                    </label>
-                    <Controller
-                      name={`weeks[${index}].is_holiday`}
-                      control={control}
-                      defaultValue={`${item.is_holiday}`}
-                      render={({ field }) => (
-                        <select
-                          className="block w-full rounded-md border bg-gray-100 border-gray-300 py-1.5 px-3 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6 disabled:cursor-not-allowed "
-                          {...field}
-                        >
-                          <option value="">--Select--</option>
-                          <option value={0}>Yes</option>
-                          <option value={1}>No</option>
-                        </select>
-                      )}
-                    />
-                    {errors?.weeks?.[index]?.is_holiday && (
-                      <span className="text-red-500 text-sm self-center">
-                        {errors.weeks[index].is_holiday.message}
-                      </span>
-                    )}
-                  </div>
-                </div>
-                <div className="flex flex-col md:flex-row gap-3 md:gap-8 md:items-center w-full">
-                  <div className="flex-1">
-                    <label className="block text-sm font-semibold font-serif leading-6 text-gray-900 mb-1 after:content-['*'] after:ml-0.5 after:text-red-600">
-                      Opening Hours
-                    </label>
-                    <Controller
-                      name={`weeks[${index}].opening_hours`}
-                      control={control}
-                      rules={{ required: "This field is required" }}
-                      defaultValue={item.opening_hours}
-                      render={({ field }) => (
-                        <input
-                          type="time"
-                          className="block w-full rounded-md border bg-gray-100 border-gray-300 py-1.5 px-3 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6 disabled:cursor-not-allowed"
-                          {...field}
-                        />
-                      )}
-                    />
-                    {errors?.opening_hours?.[index]?.opening_hours && (
-                      <span className="text-red-500 text-sm self-center">
-                        {errors.opening_hours[index].opening_hours.message}
-                      </span>
-                    )}
-                  </div>
-                </div>
-                <div className="flex flex-col md:flex-row gap-3 md:gap-8 md:items-center w-full">
-                  <div className="flex-1">
-                    <label className="block text-sm font-semibold font-serif leading-6 text-gray-900 mb-1 after:content-['*'] after:ml-0.5 after:text-red-600">
-                      Closing Hours
-                    </label>
-                    <Controller
-                      name={`weeks[${index}].closing_hours`}
-                      control={control}
-                      rules={{ required: "closing_hours field is required" }}
-                      defaultValue={item.closing_hours}
-                      render={({ field }) => (
-                        <input
-                          type="time"
-                          className="block w-full rounded-md border bg-gray-100 border-gray-300 py-1.5 px-3 text-gray-900 shadow-sm placeholder:text-gray-400 sm:text-sm sm:leading-6 disabled:cursor-not-allowed"
-                          {...field}
-                        />
-                      )}
-                    />
-                    {errors?.opening_hours?.[index]?.opening_hours && (
-                      <span className="text-red-500 text-sm self-center">
-                        {errors.opening_hours[index].opening_hours.message}
-                      </span>
-                    )}
-                  </div>
-                </div>
-                <span
-                  className="btn-primary bg-red-400 texl-sm text-gray-50 py-[9px] self-end cursor-pointer"
-                  onClick={() => remove(index)}
-                >
-                  Remove
-                </span>
-              </div>
-            ))}
-            <span
-              className="btn-primary font-serif text-sm mt-3 inline-block"
-              onClick={() =>
-                append({
-                  day: "",
-                  is_holiday: false,
-                  opening_hours: "0:00",
-                  closing_hours: "0:00",
-                })
-              }
-            >
-              Add More Holydays
-            </span>
-          </div>
           <div className="">
             <label className="block text-sm font-semibold font-serif leading-6 text-gray-900">
-              Upload your logo
+              Upload your profile picture
             </label>
             <div className="mt-1">
               <input
@@ -551,18 +330,6 @@ const SettingsForm = () => {
               file:text-sm file:font-semibold
               file:bg-color-primary/20 file:text-color-gray-200
               hover:file:bg-color-primary/30"
-              />
-            </div>
-          </div>
-          <div className="">
-            <label className="block text-sm font-semibold font-serif leading-6 text-gray-900">
-              Stock in Stock feed url
-            </label>
-            <div className="mt-1">
-              <input
-                {...register("stockfeedurl")}
-                type="url"
-                className="block w-full rounded-md border bg-gray-100 border-gray-300 py-1.5 px-3 text-gray-900 shadow-sm px-3placeholder:text-gray-400 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
--- app/otp/RecentOtp.js
+++ app/otp/RecentOtp.js
@@ -7,23 +7,29 @@ import SpinnerMini from "@/app/components/ui/SpinnerMini";
 import { useTimer } from "@gabrielyotoo/react-use-timer";
 import { useSearchParams } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { ToggleGroup, ToggleGroupItem } from "../components/ui/toggle-group";
+import { useState, useEffect } from "react";
+// import { ToggleGroup, ToggleGroupItem } from "@radix-ui/react-toggle-group";
 
 const ResendOtp = () => {
-  if (typeof window !== "undefined") {
-    var channel =
-      localStorage.getItem("channel") &&
-      JSON.parse(localStorage.getItem("channel"));
-  }
+  const [channel, setChannel] = useState(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedChannel =
+        localStorage.getItem("channel") &&
+        JSON.parse(localStorage.getItem("channel"));
+      setChannel(storedChannel);
+    }
+  }, []);
 
   const params = useSearchParams();
-  const { user, isTwoAuthEnable, isLoading } = useUser();
 
-  const { currentTime, isRunning, start } = useTimer(120, {
-    autoStart: true,
-  });
+  const { currentTime, isRunning, start } = useTimer(120);
 
   const {
     handleSubmit,
+    setValue,
     formState: { isSubmitting },
   } = useForm({
     defaultValues: {
@@ -33,6 +39,8 @@ const ResendOtp = () => {
   });
 
   async function onSubmit({ email, channel }) {
+    console.log(email, channel);
+
     try {
       const res = await resendOtp({
         channel,
@@ -53,7 +61,30 @@ const ResendOtp = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="mt-4">
+    <form onSubmit={handleSubmit(onSubmit)} className="mt-4 text-center">
+      <div className="text-center">
+        <h2 className="font-semibold font-serif mt-2">Did't get your OTP?</h2>
+        {channel && channel?.number && channel?.email && (
+          <ToggleGroup
+            type="single"
+            className="mt-2 flex flex-wrap gap-2"
+            onValueChange={(value) => setValue("channel", value)}
+          >
+            <ToggleGroupItem
+              value="number"
+              className="font-medium text-muted-foreground hover:underline hover:bg-transparent text-[15px] data-[state='on']:bg-transparent data-[state='on']:underline data-[state='on']:text-[#fcc419] p-0"
+            >
+              +88016******03
+            </ToggleGroupItem>
+            <ToggleGroupItem
+              value="email"
+              className="font-medium text-muted-foreground hover:underline hover:bg-transparent text-[15px] data-[state='on']:bg-transparent data-[state='on']:underline data-[state='on']:text-[#fcc419] p-0"
+            >
+              gr*****@gmail.com
+            </ToggleGroupItem>
+          </ToggleGroup>
+        )}
+      </div>
       <p className="text-sm font-serif">
         {isRunning && (
           <>
