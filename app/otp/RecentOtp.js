f904adb6569504e3175aef0130d3a3261990f4bc
Muhammad Akash <grixxnixx@gmail.com>
2024-07-05 00:12:23 +0600

    Update #023d

--- app/otp/RecentOtp.js
+++ app/otp/RecentOtp.js
@@ -7,9 +7,9 @@ import SpinnerMini from "@/app/components/ui/SpinnerMini";
 import { useTimer } from "@gabrielyotoo/react-use-timer";
 import { useSearchParams } from "next/navigation";
 import { useForm } from "react-hook-form";
-import { ToggleGroup, ToggleGroupItem } from "../components/ui/toggle-group";
 import { useState, useEffect } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "../components/ui/dialog";
+import { ToggleGroup, ToggleGroupItem } from "@radix-ui/react-toggle-group";
 // import { ToggleGroup, ToggleGroupItem } from "@radix-ui/react-toggle-group";
 
 const ResendOtp = () => {
@@ -63,37 +63,7 @@ const ResendOtp = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="mt-4 text-center">
-      <div className="text-center">
-        {/* {channel && channel?.number && channel?.email && (
-          <>
-            <h2 className="font-semibold font-serif mt-2">
-              Did't get your OTP?
-            </h2>
-            <p className="text-muted-foreground text-sm">
-              Choose where you want to get your OTP.
-            </p>
-
-            <ToggleGroup
-              type="single"
-              className="mt-2 flex flex-col items-start text-center gap-2"
-              onValueChange={(value) => setValue("channel", value)}
-            >
-              <ToggleGroupItem
-                value="number"
-                className="flex-1 w-full font-medium text-muted-foreground hover:border-color-[#FFB500] hover:text-[#FFB500] border hover:bg-transparent text-[15px] data-[state='on']:!bg-transparent data-[state='on']:!text-[#FFB500] data-[state='on']:!border-color-[#FFB500] m-0 h-auto"
-              >
-                Phone: {channel.number}
-              </ToggleGroupItem>
-              <ToggleGroupItem
-                value="email"
-                className="flex-1 w-full font-medium text-muted-foreground border-color-[#FFB500] hover:border-color-[#FFB500] hover:text-[#FFB500] border hover:bg-transparent text-[15px] data-[state='on']:!bg-transparent data-[state='on']:!text-[#FFB500] data-[state='on']:border-color-[#FFB500] m-0 h-auto"
-              >
-                Email: {channel.email}
-              </ToggleGroupItem>
-            </ToggleGroup>
-          </>
-        )} */}
-      </div>
+      <div className="text-center"></div>
       <p className="text-sm font-serif">
         {isRunning && (
           <>
@@ -118,16 +88,19 @@ const ResendOtp = () => {
 
               <ToggleGroup
                 type="single"
-                className="mt-2 flex flex-col items-start text-center gap-2"
+                className="mt-2 flex flex-col items-start text-center gap-4 text-sm font-medium"
                 onValueChange={(value) => setValue("channel", value)}
               >
                 <ToggleGroupItem
                   value="number"
-                  className="data-[state='on']:bg-gray-200"
+                  className="data-[state='on']:after:content-['✓'] data-[state='on']:after:ml-3 data-[state='on']:after:text-color-primary data-[state='on']:after:font-bold"
                 >
                   Phone: {channel.number}
                 </ToggleGroupItem>
-                <ToggleGroupItem value="email" className="">
+                <ToggleGroupItem
+                  value="email"
+                  className="data-[state='on']:after:content-['✓'] data-[state='on']:after:ml-3 data-[state='on']:after:text-color-primary data-[state='on']:after:font-bold"
+                >
                   Email: {channel.email}
                 </ToggleGroupItem>
               </ToggleGroup>
@@ -135,7 +108,7 @@ const ResendOtp = () => {
               <button
                 type="submit"
                 onClick={handleSubmit(onSubmit)}
-                className="mx-2 btn-primary text-sm font-medium"
+                className="btn-primary text-sm font-medium"
               >
                 {isSubmitting ? <SpinnerMini /> : "Resend"}
               </button>
