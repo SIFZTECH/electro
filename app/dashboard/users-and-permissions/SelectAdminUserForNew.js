f8d72c14625cbabc258eb09fb3126fd60037a612
Muhammad Akash <grixxnixx@gmail.com>
2024-08-04 22:36:30 +0600

    Update user form

--- app/dashboard/users-and-permissions/SelectAdminUserForNew.js
+++ app/dashboard/users-and-permissions/SelectAdminUserForNew.js
@@ -1,16 +1,16 @@
-import { UserAdminOptions } from "./UserAdminOptions";
+import { UserAdminOptionsForNew } from "./UserAdminOptionsForNew";
 
-const SelectAdminUser = ({ user, setValue }) => {
+const SelectAdminUserForNew = ({ setValue }) => {
   return (
     <div className="flex items-center gap-8">
       <div className="flex-1">
         <label className="block text-sm font-semibold font-serif leading-6 text-gray-900">
           Account Manager
         </label>
-        <UserAdminOptions user={user} setValue={setValue} />
+        <UserAdminOptionsForNew setValue={setValue} />
       </div>
     </div>
   );
 };
 
-export default SelectAdminUser;
+export default SelectAdminUserForNew;
